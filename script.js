e2f0a3c7df4a5e3f982a7cc38d9f2a18d469d1cc
Mikhail Ryazancev <alobe23@yandex.ru>
2018-09-12 16:37:19 +0300

    first commit

--- script.js
+++ script.js
@@ -11,3 +11,5 @@ document.getElementsByClassName('mini_menu_child')[0].onclick = function move_me
 		document.getElementsByClassName('move_menu_container')[0].style.left = 500 + 'px';
 	}
 }
+
+alert(window.screen.width);
